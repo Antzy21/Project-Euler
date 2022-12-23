af79f8fcaa1520ee065a4140fa8c0df362f052ee
Anthony Dunford <Anthony@Dunford.uk.com>
2022-12-23 23:39:43 +0000

    Improve Problem 12

--- Problems/Problem12.py
+++ Problems/Problem12.py
@@ -1,15 +1,23 @@
-from Functions.Divisors import getDivisors, Divisors
+from Functions.Divisors import getDivisorsBitList
+from Functions.BitArray import *
 
-S = 52403203
-L = 0
-x = 52413441-52403203
-while L < 500:
-    S += x
-    x += 1
-    #print(S)
-    D = Divisors(S)
-    L = sum(D)
-    if (L > 100):
-        print(L, S)
+def getNthTriangleNumber(n):
+    return (n+1)*n//2
 
+def getSplitDivisorListForNthTriangleNumber(n):
+    if n%2 == 0:
+        divisorList1 = getDivisorsBitList(n//2)
+        divisorList2 = getDivisorsBitList(n+1)
+    else:
+        divisorList1 = getDivisorsBitList(n)
+        divisorList2 = getDivisorsBitList((n+1)//2)
+    return divisorList1, divisorList2
 
+def solve(n):
+    L = 0
+    x = 0
+    while L < n:
+        x += 1
+        d1, d2 = getSplitDivisorListForNthTriangleNumber(x)
+        L = sum(d1)*sum(d2)
+    return getNthTriangleNumber(x)
