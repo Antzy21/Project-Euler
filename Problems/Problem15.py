4c84fac9e60da4f18573262f329e18587bc15c7a
Anthony Dunford <trashzy21@gmail.com>
2023-10-04 22:11:10 +0100

    Improve Problem 15

--- Problems/Problem15.py
+++ Problems/Problem15.py
@@ -7,7 +7,31 @@ def Lpath(x,y):
         return(1)
     return(Lpath(x-1,y)+Lpath(x,y-1))
 
-def solve():
+def solve_old():
     return Lpath(20,20)
 
 # Shame it takes 16 hours... 40 choose 20 (combinatorics or something)
+
+### 5 years later
+
+# Use a dictionary lol
+
+def solve(x = 20, y = 20):
+    
+    LpathDict = {}
+
+    def Lpath2(x,y):
+        if (x,y) in LpathDict:
+            return LpathDict[(x,y)]
+        if (y,x) in LpathDict:
+            return LpathDict[(y,x)]
+        if x == 0 or y == 0:
+            LpathDict[(x,y)] = 1
+            return 1
+
+        value = Lpath2(x-1,y)+Lpath2(x,y-1)
+        LpathDict[(x,y)] = value
+
+        return value
+
+    return Lpath2(x,y)
