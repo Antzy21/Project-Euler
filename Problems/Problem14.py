5f1eb193cfa18126debacb36fc3e48c416e1f49c
Anthony Dunford <trashzy21@gmail.com>
2023-10-04 21:58:56 +0100

    Speed up Problem 14

--- Problems/Problem14.py
+++ Problems/Problem14.py
@@ -2,14 +2,24 @@ from Functions.Collatz import Collatz
 
 def solve(b = 1000000):
     
-    X = []
-    for i in range(0, b):
-        X.append([i+1])
+    def GetCollatzLength(i):
+        if i in CollatzLengthDict:
+            return CollatzLengthDict[i]
+        else:
+            j = Collatz(i)
+            CollatzLengthDict[i] = GetCollatzLength(j) + 1
+            return CollatzLengthDict[i]
 
-    while len(X) > 1:
-        for x in X:
-            if x[-1] == 1:
-                X.remove(x)
-            x.append(Collatz(x[-1]))
-            
-    return(X[0])
+    # Initialize 1 with Collatz length of 1
+    CollatzLengthDict = {1:1}
+
+    longestCollatzLengthValue = 0
+    longestCollatzLengthKey = 1
+
+    for i in range(1, b+1):
+        GetCollatzLength(i)
+        if CollatzLengthDict[i] > longestCollatzLengthValue:
+            longestCollatzLengthValue = CollatzLengthDict[i]
+            longestCollatzLengthKey = i
+
+    return longestCollatzLengthKey
