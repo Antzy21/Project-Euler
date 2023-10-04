32681d0e2e92408a865470b3c3c4b7bd39a75aae
Anthony Dunford <trashzy21@gmail.com>
2023-10-04 22:45:42 +0100

    Fix Problem21

--- Functions/Divisors.py
+++ Functions/Divisors.py
@@ -3,11 +3,11 @@ from Functions.BitArray import NumsFromBits
 
 def getDivisorsBitList(n):
     sqrtN = ceil(sqrt(n))
-    divisorsList = [0]*n
-    for x in range(1, sqrtN+1):
+    divisorsList = [0]*(n+1)
+    for x in range(1,sqrtN+1):
         if n % x == 0:
-            divisorsList[x-1] = 1
-            divisorsList[n//x-1] = 1
+            divisorsList[x] = 1
+            divisorsList[n//x] = 1
     return divisorsList
 
 def getDivisors(n):
@@ -18,9 +18,5 @@ def getProperDivisors(n):
     if n <= 1:
         return []
     divisorsBitList = getDivisorsBitList(n)
-    divisorsBitList[-1]=0
+    divisorsBitList[-1] = 0
     return NumsFromBits(divisorsBitList)
-
-def sumOfProperDivisors(n):
-    properDivisors = getProperDivisors(n)
-    return sum(properDivisors)
--- Problems/Problem21.py
+++ Problems/Problem21.py
@@ -1,14 +1,13 @@
-from Functions.Divisors import sumOfProperDivisors
+from Functions.Divisors import getProperDivisors
 from Functions.DigitsArithmatic import AddDigits
 
 def solve(n = 10000):
     amicableNumbers = []
 
     for i in range(1,n+1):
-        j = sumOfProperDivisors(i)
-        if sumOfProperDivisors(j) == i:
-            if i != j:
-                amicableNumbers.append(i)
-                amicableNumbers.append(j)
+        j = sum(getProperDivisors(i))
+        if i > j and sum(getProperDivisors(j)) == i:
+            amicableNumbers.append(i)
+            amicableNumbers.append(j)
 
-    return(sum(amicableNumbers)//2)
+    return(sum(amicableNumbers))
