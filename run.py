d5c5ccd34f3951027b1d00b95f5ea4c794f06950
Anthony Dunford <Anthony.Dunford@Verifaro.co.uk>
2023-11-07 16:06:14 +0000

    Load the solvers dynamically

--- run.py
+++ run.py
@@ -1,30 +1,6 @@
-from Problems import Problem2
-from Problems import Problem3
-from Problems import Problem4
-from Problems import Problem5
-from Problems import Problem6
-from Problems import Problem7
-from Problems import Problem8
-from Problems import Problem9
-from Problems import Problem10
-from Problems import Problem11
-from Problems import Problem12
-from Problems import Problem13
-from Problems import Problem14
-from Problems import Problem15
-from Problems import Problem16
-from Problems import Problem17
-from Problems import Problem18
-from Problems import Problem19
-from Problems import Problem20
-from Problems import Problem21
-from Problems import Problem22
-from Problems import Problem23
-from Problems import Problem24
-from Problems import Problem67
-
+from importlib import import_module
 import time
-from Functions.Divisors import getProperDivisors
+import os
 
 def printSolver(func, i):
     t1 = time.perf_counter()
@@ -32,36 +8,17 @@ def printSolver(func, i):
     t2 = time.perf_counter()
     print(f"Problem {i:3}: {result:12} - {round(t2-t1,2)}s")
 
-problems = {
-    2: Problem2.solve,
-    3: Problem3.solve,
-    4: Problem4.solve,
-    5: Problem5.solve,
-    6: Problem6.solve,
-    7: Problem7.solve,
-    8: Problem8.solve,
-    9: Problem9.solve,
-    10: Problem10.solve,
-    11: Problem11.solve,
-    12: Problem12.solve,
-    13: Problem13.solve,
-    14: Problem14.solve,
-    15: Problem15.solve,
-    16: Problem16.solve,
-    17: Problem17.solve,
-    18: Problem18.solve,
-    19: Problem19.solve,
-    20: Problem20.solve,
-    21: Problem21.solve,
-    22: Problem22.solve,
-    23: Problem23.solve,
-    24: Problem24.solve,
-    67: Problem67.solve,
-}
-
-# for problemNumber in problems:
-#     solveFunction = problems[problemNumber]
-#     printSolver(solveFunction, problemNumber)
-
-problemNumber = 10
-printSolver(problems[problemNumber], problemNumber)
+# Get all the problem solvers dynamically
+for file in os.listdir("Problems"):
+    
+    if ".py" not in file:
+        continue
+    
+    problemName = file.removesuffix(".py")
+    problemNumber = int(problemName.removeprefix("Problem"))
+    
+    module = import_module(f"Problems.{problemName}")
+    solveFunction = getattr(module, "solve")
+    
+    # Run the solve function
+    printSolver(solveFunction, problemNumber)
