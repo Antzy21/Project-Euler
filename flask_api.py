1c641cdefb15eaef50e3ed234c451b1203069441
Anthony Dunford <trashzy21@gmail.com>
2023-11-08 21:17:16 +0000

    Get problems for flask app from run.py

--- flask_api.py
+++ flask_api.py
@@ -1,56 +1,10 @@
-from Problems import Problem2
-from Problems import Problem3
-from Problems import Problem4
-from Problems import Problem5
-from Problems import Problem6
-from Problems import Problem7
-from Problems import Problem8
-from Problems import Problem9
-from Problems import Problem11
-from Problems import Problem12
-from Problems import Problem13
-from Problems import Problem14
-from Problems import Problem15
-from Problems import Problem16
-from Problems import Problem17
-from Problems import Problem18
-from Problems import Problem19
-from Problems import Problem20
-from Problems import Problem21
-from Problems import Problem22
-from Problems import Problem23
-from Problems import Problem24
-from Problems import Problem67
-
 from flask import Flask, json
+from importlib import import_module
+from run import getProblemSolvers
+import time
+import os
 
 api = Flask(__name__)
-
-problems = {
-    2: Problem2.solve,
-    3: Problem3.solve,
-    4: Problem4.solve,
-    5: Problem5.solve,
-    6: Problem6.solve,
-    7: Problem7.solve,
-    8: Problem8.solve,
-    9: Problem9.solve,
-    11: Problem11.solve,
-    12: Problem12.solve,
-    13: Problem13.solve,
-    14: Problem14.solve,
-    15: Problem15.solve,
-    16: Problem16.solve,
-    17: Problem17.solve,
-    18: Problem18.solve,
-    19: Problem19.solve,
-    20: Problem20.solve,
-    21: Problem21.solve,
-    22: Problem22.solve,
-    23: Problem23.solve,
-    24: Problem24.solve,
-    67: Problem67.solve,
-}
 
 @api.route('/')
 def index():
@@ -75,4 +29,7 @@ def get_solution(numberStr):
     return json.dumps(jsonObj)
 
 if __name__ == "__main__":
+
+    problems = getProblemSolvers()
+
     api.run(host ='0.0.0.0', port = 5000, debug = True)
--- run.py
+++ run.py
@@ -2,23 +2,35 @@ from importlib import import_module
 import time
 import os
 
-def printSolver(func, i):
+def _printSolver_(func, i):
     t1 = time.perf_counter()
     result = func()
     t2 = time.perf_counter()
     print(f"Problem {i:3}: {result:12} - {round(t2-t1,2)}s")
 
-# Get all the problem solvers dynamically
-for file in os.listdir("Problems"):
+def getProblemSolvers():
+    problems = {}
+
+    # Get all the problem solvers dynamically
+    for file in os.listdir("Problems"):
+        
+        if ".py" not in file:
+            continue
+        
+        problemName = file.removesuffix(".py")
+        problemNumber = int(problemName.removeprefix("Problem"))
+        
+        module = import_module(f"Problems.{problemName}")
+        solveFunction = getattr(module, "solve")
+        
+        problems[problemNumber] = solveFunction
+
+    return problems
+
+if __name__ == "__main__":
     
-    if ".py" not in file:
-        continue
-    
-    problemName = file.removesuffix(".py")
-    problemNumber = int(problemName.removeprefix("Problem"))
-    
-    module = import_module(f"Problems.{problemName}")
-    solveFunction = getattr(module, "solve")
-    
-    # Run the solve function
-    printSolver(solveFunction, problemNumber)
+    problems = getProblemSolvers()
+
+    for problemNumber in problems:
+        solveFunction = problems[problemNumber]
+        _printSolver_(solveFunction, problemNumber)
